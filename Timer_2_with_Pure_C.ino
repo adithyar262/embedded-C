3721ade23681a4590d814a73f401c5f989e987a5
adithyar262 <39980616+adithyar262@users.noreply.github.com>
2018-06-19 14:38:17 +0530

    Code after proper styling.

--- Timer_2_with_Pure_C.ino
+++ Timer_2_with_Pure_C.ino
@@ -15,38 +15,34 @@ An LED connected to PC0(A0) is used to show the output.
 #include <avr/interrupt.h>
 
 //Variable to count the number of overflows  
-volatile uint8_t totaloverflow; 
+volatile uint8_t total_overflow; 
   
-//Initialization of the timer, interrupt and variable
-
-void timer()
-{
-    TCCR2B |= (1 << CS22)|(1 << CS21);   //Setting prescaler = 256
-    TCNT2 = 0;                           //Initialize counter
-    TIMSK2 |= (1 << TOIE2);              //Enable overflow interrupt
-    sei();                               //Enable global interrupts
-    totaloverflow = 0;                   //Initializing overflow counter variable
+//Function for initialization of the timer, interrupt and variable
+void timer() {
+  TCCR2B |= (1 << CS22)|(1 << CS21);  //Setting prescaler = 256
+  TCNT2 = 0;                          //Initialize counter
+  TIMSK2 |= (1 << TOIE2);             //Enable overflow interrupt
+  sei();                              //Enable global interrupts
+  total_overflow = 0;                 //Initializing overflow counter variable
 }
 
 //ISR called whenever TCNT0 overflows
-
-ISR(TIMER2_OVF_vect)
-{
-    totaloverflow++;
+ISR(TIMER2_OVF_vect) {
+  total_overflow++;
 }
 
-// Main function, checks whether totaloverflow = 3 and resets the Timer
-
+// Main function, checks whether total_overflow = 3 and resets the Timer
 int main(void){
-    DDRC |= (1 << 0);
-    timer();
-    while(1){
-        if (totaloverflow >= 3){     //Checking if no. of overflows = 3
-            if (TCNT2 >= 80){        //Checking if the timer count reaches 80
-                PORTC ^= (1 << 0);   // toggles the led
-                TCNT2 = 0;           // reset counter
-                totaloverflow = 0;   // reset overflow counter
-            }
-        }
+  DDRC |= (1 << 0);
+  timer();  //Calls function to initialize the timer and set the prescalar
+  
+  while(1){
+    if (total_overflow >= 3){  //Checking if no. of overflows = 3
+      if (TCNT2 >= 80){        //Checking if the timer count reaches 80
+        PORTC ^= (1 << 0);     // toggles the led
+        TCNT2 = 0;             // reset counter
+        total_overflow = 0;    // reset overflow counter
+      }
     }
+  }
 }
