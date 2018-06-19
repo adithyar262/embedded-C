b0e238c5daa76035d09a55c1da532b31a40d01db
adithyar262 <39980616+adithyar262@users.noreply.github.com>
2018-06-19 14:25:44 +0530

    Update Analog_Output_Using_Pure_C.ino
    Code after proper styling.

--- Analog_Output_Using_Pure_C.ino
+++ Analog_Output_Using_Pure_C.ino
@@ -3,7 +3,7 @@
 
 /*
                               Analog Output
- Demonstrates Analog Output by connecting the speed of a DC Motor connected
+ Demonstrates Analog Output by controlling the speed of a DC Motor connected
  to Analog Pin A0 with a delay of 1 second.
   
 */
@@ -11,18 +11,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-//Main funtion, sets pin A0 as Output
-
+//Main funtion, sets pin A0 as output and runs the infinite loop.
 int main(){
   DDRC|=_BV(DDC0);
 
   //Infinite loop to control speed of motor
-  
   while(1){
-  PORTC|=_BV(PC0);
-  _delay_ms(1000);
-  PORTC&=~_BV(PC0);
-  _delay_ms(1000);
+    PORTC|=_BV(PC0);
+    _delay_ms(1000);
+    PORTC&=~_BV(PC0);
+    _delay_ms(1000);
   }
   return 0;
 }
