7b64a0173b56ba72c7ddcc6965b9ffa08afaac44
adithyar262 <39980616+adithyar262@users.noreply.github.com>
2018-06-19 14:31:21 +0530

    Code after proper styling.

--- Counter_with_Arduino.ino
+++ Counter_with_Arduino.ino
@@ -9,32 +9,28 @@
  
 */
 
-bool state=1;
+//Boolean variable to set the initial value as 1
+bool state=1;  
 
-//Sets up Timer and Interrupt
-
+//Sets up Timer, Interrupt and pin 13 as OUTPUT
 void setup() {
   pinMode(13,OUTPUT);
-    
-  TCCR0A=(1<<WGM01);                //Sets in CTC mode   
-  OCR0A=0xF9;                       //Value for ORC0A for 1ms 
+  TCCR0A=(1<<WGM01);   //Sets in CTC mode   
+  OCR0A=0xF9;          //Value for ORC0A for 1ms 
   TIMSK0|=(1<<OCIE0A);   
   sei();
-  TCCR0B|=(1<<CS01);                //Sets the prescalar as 64
+  TCCR0B|=(1<<CS01);   //Sets the prescalar as 64
   TCCR0B|=(1<<CS00); 
 }
 
-// Assigns state value to pin 13
-
+//Infinite loop to assign state value to Pin 13
 void loop() {
  digitalWrite(13,state);
 }
 
-//ISR called on overflows
-
-ISR(TIMER0_COMPA_vect){   
+//ISR called on overflows, sets Pin 13 as LOW for 0.5 seconds
+ISR(TIMER0_COMPA_vect) {   
   digitalWrite(13,LOW);
   delay(500);
   digitalWrite(13,state); 
 }
-
