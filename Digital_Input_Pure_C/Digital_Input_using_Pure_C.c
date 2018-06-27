3dffb1fdfd25064ba42ebcdfd6f111cb8ecab4d6
adithyar262 <39980616+adithyar262@users.noreply.github.com>
2018-06-27 18:16:13 +0530

    with Makefile

---
+++ Digital_Input_Pure_C/Digital_Input_using_Pure_C.c
@@ -0,0 +1,30 @@
+// Post: "Digital I/O - Digital Input using Pure C"
+// Author: Adithya Rajeev
+
+/*
+                          Digital Input
+ Demonstrates Digital Input by switching ON and OFF an LED connected
+ to Digital Pin 13 whenever the Push Button connected to Digital Pin 2
+ is pressed.
+ 
+*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+//Main function, it sets Pin 13 as Output and Pin 2 as Input and runs
+//the infinite loop.
+int main() {
+  DDRB|=_BV(DDB5); 
+  DDRD &= ~_BV(DDD2);
+  PORTD |= _BV(PD2);
+
+  // Infinite loop to check button state and toggle LED
+  while (1) {
+    if (PIND & PORTD)
+      PORTB &= ~_BV(PB5);
+    else
+      PORTB |= _BV(PB5);
+  }
+  return 0;
+}
